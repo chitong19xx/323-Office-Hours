5bc8abe769e853f1305b9ffcace1b4e65127d084
Lee Danilek <lee@Lees-MacBook-Pro.local>
2017-11-25 22:37:15 -0500

    .

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask
+from flask import request
+import json
 
 DATA_PATH = "data.txt"
 
-def get_number_in_queue():
+def get_data():
     try:
         with open(DATA_PATH, "r") as f:
-            for l in f:
-                return int(l.strip())
+            return json.load(f)
     except IOError:
         pass
-    return 0
+    return {'size': 0, 'list': []}
+
+def get_number_in_queue():
+    return get_data()['size']
 
 def set_number_in_queue(num):
+    old_data = get_data()
+    old_data['size'] = num
     with open(DATA_PATH, "w") as f:
-        f.write(str(num) + "\n")
+        json.dump(old_data, f)
 
 app = Flask(__name__)
 @app.route('/')
@@ -24,6 +30,11 @@ def report_queue_size():
 def change_queue_size(size):
     set_number_in_queue(size)
     return "Done"
+
+@app.route('/set_list', methods=['POST'])
+def set_list():
+    json = request.get_json(force=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
