8694b146fa2af01d112d97310b27803c764cd05c
Lee Danilek <lee@Lees-MacBook-Pro.local>
2017-11-25 22:14:39 -0500

    no globals, database file

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask
 
+DATA_PATH = "data.txt"
+
 number_in_queue = 0
+
+def get_number_in_queue():
+	with open(DATA_PATH, "r") as f:
+		for l in f:
+			return int(l.strip())
+	return 0
+
+def set_number_in_queue(num):
+	with open(DATA_PATH, "w") as f:
+		f.write(str(num) + "\n")
 
 app = Flask(__name__)
 @app.route('/')
 def report_queue_size():
-    return "The queue has "+str(number_in_queue)+" people in it."
+    return "The queue has "+str(get_number_in_queue())+" people in it."
 
 @app.route('/change/<int:size>')
 def change_queue_size(size):
-    global number_in_queue
-    number_in_queue = size
+	set_number_in_queue(size)
     return "Done"
 
 if __name__ == '__main__':
